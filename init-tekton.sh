46aea1cd20fcdb8cdb941f3db75cec86a50926b4
Guido <guidopercu@gmail.com>
2015-06-25 10:58:53 -0300

    Retirando comandos que não funcionam

--- init-tekton.sh
+++ init-tekton.sh
@@ -4,6 +4,9 @@ read project_name
 wget https://github.com/renzon/tekton/archive/master.zip
 unzip master && rm master.zip
 mv tekton-master $project_name && cd $project_name
+
+echo "Instaling... "
 cd backend/venv/ && ./venv.sh
-source ./bin/activate
-cd ../appengine && dev_appserver.py .
+
+echo "You should configure your app in app.yaml and settings.py"
+echo "Done!"
