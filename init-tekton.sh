6f737352505ec5fd28ecc154ba7352ac37a0301c
Guido <guidopercu@gmail.com>
2016-02-07 16:10:20 -0200

    Separando frontend de backend

--- init-tekton.sh
+++ init-tekton.sh
@@ -9,6 +9,14 @@ git init
 echo "Instaling... "
 cd backend/venv/ && ./venv.sh
 cd ../../
+mkdir -p frontend/static
+mkdir -p frontend/templates
+nv backend/appengine/static/* frontend/static/
+nv backend/appengine/templates/* frontend/templates/
+rm -rf backend/appengine/static/
+rm -rf backend/appengine/templates/
+ln -s frontend/static/ backend/appengine/static/
+ln -s frontend/templates/ backend/appengine/templates/
 
 git add --all
 git commit -m 'First commit'
